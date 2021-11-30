b94d41a443eb3e9a46ed321aa2a85296166c7f6b
jeigar2 <jeigar2@gmail.com>
2021-11-30 06:12:38 +0100

    php resultados enero 2018
    Escribe todos los resultado de enero de 2018

---
+++ resultados_deportivos.php
@@ -0,0 +1,23 @@
+<?php
+$file = fopen("../files/results.csv","r");
+
+$match   = 0;
+$nomatch = 0;
+
+while(!feof($file)) {
+    $line = fgets($file);
+    if(preg_match(
+        '/^2018\-01\-(\d\d),.*$/',
+        $line,
+        $m
+      )
+    ) {
+        print_r($m);
+        $match++;
+    } else {
+        $nomatch++;
+    }
+}
+fclose($file);
+
+printf("\n\nmatch: %d\nnomatch: %d\n", $match, $nomatch);
