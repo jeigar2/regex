213df313d9ecc3ba9b81113919ab9602bf5492dc
jeigar2 <jeigar2@gmail.com>
2021-11-30 06:21:13 +0100

    revisar los resultados
    indica si gana local, visitante o fue empate

--- resultados_deportivos.php
+++ resultados_deportivos.php
@@ -4,15 +4,24 @@ $file = fopen("../files/results.csv","r");
 $match   = 0;
 $nomatch = 0;
 
+$t = time();
+
 while(!feof($file)) {
     $line = fgets($file);
     if(preg_match(
-        '/^2018\-01\-(\d\d),.*$/',
+        '/^(\d{4}\-\d\d\-\d\d),(.+),(.+),(\d+),(\d+),.*$/i',
         $line,
         $m
       )
     ) {
-        print_r($m);
+        if ($m[4] == $m[5]) {
+            printf("empate: ");
+        } elseif ($m[4] > $m[5]) {
+            echo "local:  ";
+        } else {
+            echo "visitante: "
+        }
+        printf("\t%s, %s [%d-%d]\n", $m[2], $m[3], $m[4], $m[5]);
         $match++;
     } else {
         $nomatch++;
@@ -21,3 +30,5 @@ while(!feof($file)) {
 fclose($file);
 
 printf("\n\nmatch: %d\nnomatch: %d\n", $match, $nomatch);
+
+printf("tiempo: %d\n", time() - $t);
